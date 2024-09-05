8fefe6c51a30a4a1b094457ab8bcb39b77631b9e
TechBuilderTeam <saashid18@gmail.com>
2024-09-05 23:31:30 +0600

    csrf fixed

--- social_media_server/settings.py
+++ social_media_server/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    'https://social-media-drf.onrender.com/',  'https://*.127.0.0.1',
+    'https://social-media-drf.onrender.com',  'https://*.127.0.0.1',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
